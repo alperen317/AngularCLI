bcf3dd94d8734068e3665281bfac18f6c0386151
alperen <alialperena@gmail.com>
2019-09-05 10:15:24 +0300

    Project1 update

--- project1/README.md
+++ project1/README.md
@@ -1,6 +1,6 @@
-# Project1
+# MyFirstApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.0.
 
 ## Development server
 
--- project1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipes.model';
+
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,16 +10,17 @@ import { Recipe } from '../recipes.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[];
 
-  recipes : Recipe[] = [
-    new Recipe('A test Recipe', 'This. is simply a test',  'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/crab-asparagus-pappardelle.jpg'),
-    new Recipe('A test Recipe', 'This. is simply a test',  'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/crab-asparagus-pappardelle.jpg'),
-    new Recipe('A test Recipe', 'This. is simply a test',  'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/crab-asparagus-pappardelle.jpg')
-  ];
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private recipeService: RecipeService,
+              private router: Router,
+              private route: ActivatedRoute) {
   }
 
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
+  onNewRecipe(){
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
 }
